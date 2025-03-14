95b9c2527f15db2bd6926723b716d1cba0915ed0
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-03-15 00:13:18 +0200

    Added a new Python file.

---
+++ ollama_gemma2_fte.py
@@ -0,0 +1,24 @@
+from ollama import Client
+
+
+def main():
+    """
+    Send user request to the pre-chosen Ollama model.
+    """
+    input_prompt = input("Please, enter your prompt (in the terminal): \n")
+    print("Waiting for SLM's response... \n")
+
+    try:
+        client = Client(host = 'http://localhost:11434')
+        response = client.generate(model = 'gemma2:2b', prompt = input_prompt)
+        print(f"Gemma's message: \n{response['response']} \n")
+
+    except RuntimeError as e:
+        print(f"Runtime Error: {e}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
