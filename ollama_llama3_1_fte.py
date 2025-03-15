20823ce46eddf7e79cc38b1f290d8b15d061583a
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-03-15 18:13:18 +0200

    Added a new Python file.

---
+++ ollama_llama3_1_fte.py
@@ -0,0 +1,24 @@
+from ollama import Client
+
+
+def main():
+    """
+    Send user request to the pre-chosen Ollama model.
+    """
+    input_prompt = input("Please, enter your prompt (in the terminal): \n")
+    print("Waiting foor SLM's response... \n")
+
+    try:
+        client = Client(host = 'http://localhost:11434')
+        response = client.generate(model = 'llama3.1:8b', prompt = input_prompt)
+        print(f"Llama's message: \n{response['response']} \n")
+
+    except RuntimeError as e:
+        print(f"Runtime Error: {e}")
+
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+
+
+if __name__ == "__main__":
+    main()
